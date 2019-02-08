4bc75a8978562761b8cd6ae8f53384dfcea32052
Ho Yan Leung <hy.leung@gmail.com>
2019-02-08 13:19:37 -0800

    Save to Dynamo

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,18 @@
 import { Context } from "aws-lambda";
+import AWS from "aws-sdk";
 import pino from "pino";
+import { ulid } from "ulid";
 
 const logger = pino();
+const dynamoClient = new AWS.DynamoDB.DocumentClient();
 export const handler = (event: any, context: Context) => {
   logger.info("event received", { ...event, ...context });
+  dynamoClient
+    .put({
+      TableName: "ts-lambda-xray",
+      Item: { key: ulid(), payload: event }
+    })
+    .promise()
+    .then(r => logger.info("persisted", r))
+    .catch(e => logger.error(e));
 };
